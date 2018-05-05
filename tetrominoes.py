ea18c4fae867670c295481456e6b95aaa4a57a8d
Que Huong <luquehuong@gmail.com>
2018-05-05 16:36:49 +0700

    finish homework_4

--- tetrominoes.py
+++ tetrominoes.py
@@ -31,10 +31,63 @@ class I_shape(Shape):
                   Point(center.x + 1, center.y)]
         Shape.__init__(self, coords, "blue")
 
+class J_shape(Shape):
+    def __init__(self, center):
+        coords = [Point(center.x - 1, center.y),
+                  Point(center.x    , center.y),
+                  Point(center.x + 1, center.y),
+                  Point(center.x + 1, center.y + 1)]
+        Shape.__init__(self, coords, "orange1")
+
+class L_shape(Shape):
+    def __init__(self, center):
+        coords = [Point(center.x - 1, center.y + 1),
+                  Point(center.x - 1, center.y),
+                  Point(center.x    , center.y),
+                  Point(center.x + 1, center.y)]
+        Shape.__init__(self, coords, "pink")
+
+class O_shape(Shape):
+    def __init__(self, center):
+        coords = [Point(center.x - 1, center.y),
+                  Point(center.x - 1, center.y + 1),
+                  Point(center.x    , center.y + 1),
+                  Point(center.x    , center.y)]
+        Shape.__init__(self, coords, "cyan")
+
+class S_shape(Shape):
+    def __init__(self, center):
+        coords = [Point(center.x - 1, center.y + 1),
+                  Point(center.x    , center.y + 1),
+                  Point(center.x    , center.y),
+                  Point(center.x + 1, center.y)]
+        Shape.__init__(self, coords, "GreenYellow")
+
+class T_shape(Shape):
+    def __init__(self, center):
+        coords = [Point(center.x - 1, center.y),
+                  Point(center.x    , center.y),
+                  Point(center.x + 1, center.y),
+                  Point(center.x    , center.y + 1)]
+        Shape.__init__(self, coords, "red")
+
+class Z_shape(Shape):
+    def __init__(self, center):
+        coords = [Point(center.x - 1, center.y),
+                  Point(center.x    , center.y),
+                  Point(center.x    , center.y + 1),
+                  Point(center.x + 1, center.y + 1)]
+        Shape.__init__(self, coords, "yellow")
+
 def main():
-    win = GraphWin("Tetrominoes", 200, 150)
-    shape = I_shape(Point(3, 1))
-    shape.draw(win)
+    win = GraphWin("Tetrominoes", 900, 150)
+    tetrominoes = [I_shape, J_shape, L_shape, O_shape, S_shape, T_shape, Z_shape]
+    x = 3
+    for tetromino in tetrominoes:
+        shape = tetromino(Point(x, 1))
+        shape.draw(win)
+        x += 4
+    win.mainloop()
     win.mainloop()
 
 main()
